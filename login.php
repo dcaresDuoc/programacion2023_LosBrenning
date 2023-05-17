816d79a278ec7858f9158ddd7b2cddc73cec5598
Skinnyflakkk <107156403+Skinnyflakkk@users.noreply.github.com>
2023-05-17 18:24:28 -0400

    up

--- login.php
+++ login.php
@@ -3,13 +3,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    // Verificar las credenciales en la base de datos (reemplaza con tu lógica de verificación de credenciales)
+    // Verificar las credenciales en la base de datos
     if ($username === 'usuario' && $password === 'contraseña') {
         // Credenciales válidas, establecer la sesión
         $_SESSION['username'] = $username;
 
-        // Redirigir al usuario a la página de inicio de sesión exitoso o a otra página protegida
-        header('Location: inicio.php');
         exit();
     } else {
         // Credenciales inválidas, mostrar mensaje de error
