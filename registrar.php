5225f8a0833f8bf6827af3bebd288168b3227a7b
Francisco Egenau Navia <fr.egenau@duocuc.cl>
2023-05-17 14:37:44 -0400

    Se realiza un nuevo formulario con la conexion establecida a mysql +ç

---
+++ registrar.php
@@ -0,0 +1,29 @@
+<?php
+
+$usuario = isset($_POST['txt_usuario']) ? $_POST['txt_usuario'] : '';
+$nombre = isset($_POST['txt_nombre']) ? $_POST['txt_nombre'] : '';
+$contraseña = isset($_POST['txt_contraseña']) ? $_POST['txt_contraseña'] : '';
+try{
+    
+    $conexion new PDO('
+        mysql:host=localhost;
+        port=3306;
+        dbname=prueba1_db',
+        'root',
+        '');
+    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERROMODE_WARNING);
+
+    $pdo = $conexion->prepare('INSERT INTO usuarios(usuario,nombre,contraseña) VALUES (?, ?, ?)');
+    $pdo->bindParam(1,$usuario);
+    $pdo->bindParam(2,$nombre);
+    $pdo->bindParam(3,$contraseña);
+    $pdo->execute() or die (print($pdo->errorInfo())); 
+
+    echo json_encode('true')
+    
+}catch(PDOException $error){
+    echo $error -> getMessage();
+    die();
+} 
+
