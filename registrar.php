8274c7f8fcdb60d06b1d74e1506a82fd18b67738
Francisco Egenau Navia <fr.egenau@duocuc.cl>
2023-05-17 18:35:16 -0400

    cambios a la pagina

--- registrar.php
+++ registrar.php
@@ -2,7 +2,9 @@
 
 $usuario = isset($_POST['txt_usuario']) ? $_POST['txt_usuario'] : '';
 $nombre = isset($_POST['txt_nombre']) ? $_POST['txt_nombre'] : '';
+$correo = isset($_POST['txt_correo']) ? $_POST['txt_correo'] : '';
 $contraseña = isset($_POST['txt_contraseña']) ? $_POST['txt_contraseña'] : '';
+
 try{
     
     $conexion new PDO('
@@ -14,10 +16,11 @@ try{
     $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERROMODE_WARNING);
 
-    $pdo = $conexion->prepare('INSERT INTO usuarios(usuario,nombre,contraseña) VALUES (?, ?, ?)');
+    $pdo = $conexion->prepare('INSERT INTO usuarios(usuario,nombre,contraseña) VALUES (?, ?, ?, ?)');
     $pdo->bindParam(1,$usuario);
     $pdo->bindParam(2,$nombre);
     $pdo->bindParam(3,$contraseña);
+    $pdo->bindParam(4,$correo);
     $pdo->execute() or die (print($pdo->errorInfo())); 
     
 }catch(PDOException $error){
