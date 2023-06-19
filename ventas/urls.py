e724ca17e6249168390c991c0f06aff92fe7ff27
Francisco Egenau Navia <fr.egenau@duocuc.cl>
2023-06-19 02:06:11 -0400

    cambios

--- ventas/urls.py
+++ ventas/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('modelos/', views.modelos),
+    path('productos/', views.productos),
+    path('contacto/', views.contacto),
 ]
--- ventas/views.py
+++ ventas/views.py
@@ -3,4 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 
 def index (request):
-    return render(request,'index.html')
+    title = 'Brenning Motors'
+    return render(request,'index.html',{
+        'title': title
+    })
+
+def modelos (request):
+    return render(request,'modelos.html')
+
+def productos (request):
+    return render(request,'productos.html')
+
+def contacto (request):
+    return render(request,'contacto.html')
