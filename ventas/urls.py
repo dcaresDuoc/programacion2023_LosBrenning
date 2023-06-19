7f3ed11c9e76e698c67f1d2823143ee3010e5503
Francisco Egenau Navia <fr.egenau@duocuc.cl>
2023-06-19 15:32:37 -0400

    borro app ventas, por anexion de ambiente virtual ya creado

--- ventas/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index),
-    path('modelos/', views.modelos),
-    path('productos/', views.productos),
-    path('contacto/', views.contacto),
-]
